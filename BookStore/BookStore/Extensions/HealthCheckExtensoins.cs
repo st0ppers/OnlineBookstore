27b1bd0506b59a0edb7a1ed48095c84283416039
Alexander <a.damov@draftkings.com>
2022-10-05 12:01:25 +0300

    Added HealhtChecks

---
+++ BookStore/BookStore/Extensions/HealthCheckExtensoins.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+using BookStore.Models.Models.Healthchecks;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+
+namespace BookStore.Extensions
+{
+    public static class HealthCheckExtensions
+    {
+        public static IApplicationBuilder RegisterHealthChecks(this IApplicationBuilder app)
+        {
+
+            return app.UseHealthChecks("/healthz", new HealthCheckOptions()
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    context.Response.ContentType = "application/json";
+                    var response = new HealthCheckResponse
+                    {
+                        Status = report.Status.ToString(),
+                        HealhtChecks = report.Entries.Select(x=> new IndividualHealthCheckResponses()
+                        {
+                            Component = x.Key,
+                            Status = x.Value.Status.ToString(),
+                            Description = x.Value.Description
+                        }),
+                        HealthCheckDuration = report.TotalDuration
+                    };
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(response, Formatting.Indented));
+                }
+            });
+        }
+    }
+}
---
+++ BookStore/BookStore/HealthChecks/TestHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BookStore.HealthChecks
+{
+    public class TestHealthCheck : IHealthCheck
+    {
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                return HealthCheckResult.Unhealthy($"Test dead : {e.Message}");
+            }
+            return HealthCheckResult.Healthy("Test is healthy");
+        }
+    }
+}
